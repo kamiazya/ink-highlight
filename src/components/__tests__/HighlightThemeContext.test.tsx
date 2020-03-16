3a5e52aab3ac569ac8020056dac44a41063af801
kamiazya <yuki@kamiazya.tech>
2020-03-16 23:07:51 +0900

    add tests for HighlightThemeContext

---
+++ src/components/__tests__/HighlightThemeContext.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { wrap } from 'jest-snapshot-serializer-raw';
+import { render } from 'ink-testing-library';
+import { Theme } from 'cli-highlight';
+import { Highlight } from '../Highlight';
+import { HighlightThemeContext } from '../HighlightThemeContext';
+
+describe('HighlightThemeContext', () => {
+  test('Theme applied', () => {
+    const theme = { test: true } as Theme;
+    const { lastFrame } = render(
+      <HighlightThemeContext.Provider value={theme}>
+        <Highlight code="console.log('Hello world!');" />
+      </HighlightThemeContext.Provider>,
+    );
+    expect(wrap(lastFrame())).toMatchSnapshot();
+  });
+});
